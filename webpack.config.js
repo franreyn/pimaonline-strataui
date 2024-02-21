d8d672f8e93e5c514af3505e8a676f4cf00b6cd5
aphan-webdev <aphan@pima.edu>
2024-02-21 09:10:45 -0700

    Update webpack directory path

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, '/'),
     },
     open: true, // open browser when server starts
     compress: true,
