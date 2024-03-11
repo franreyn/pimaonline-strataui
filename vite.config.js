513e887ae9160140092ffaace49cad790efcd009
aphan-webdev <aphan@pima.edu>
2024-03-11 14:19:09 -0700

    Remove hashes from file names

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  build: { outDir: '../dist' },
+  build: {
+    outDir: '../dist',
+    assetsDir: 'assets',
+    // rollupOptions is used to remove hashes from file names
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
   root: './src',
   server: {
     host: true,
