e92002861cf3783f496696a42a926bc3ea3f9ab6
Clint Mossman <cmossman@pima.edu>
2024-04-05 13:47:47 -0700

    Added typescripting for aria.ts

--- src/ts/modules/aria.ts
+++ src/ts/modules/aria.ts
@@ -4,19 +4,21 @@ console.log(m);
 
 
 // Function to add aria label to custom elements
-customElementAria.forEach((customElement) => {
+customElementAria.forEach((customElement: { customHtmlTag: string, ariaProp: string, ariaValue: string }) => {
 
   // For each custom element, find all instances on the page
-  let customHtmlAriaElements = document.querySelectorAll(customElement.customHtmlTag);
+  let customHtmlAriaElements: NodeListOf<HTMLElement> = document.querySelectorAll(customElement.customHtmlTag);
 
-  customHtmlAriaElements.forEach((customHtmlAriaElement) => {
-    // For each instance, set the corresponding aria attribute and value
-    if (customHtmlAriaElement instanceof HTMLElement) {
-      customHtmlAriaElement.setAttribute(customElement.ariaProp, customElement.ariaValue);
-    } else {
-      console.error("Element is not an HTMLElement:", customHtmlAriaElement);
-    }
+  if (customHtmlAriaElements.length > 0) {
+    customHtmlAriaElements.forEach((customHtmlAriaElement: HTMLElement) => {
+      // For each instance, set the corresponding aria attribute and value
+      if (customHtmlAriaElement instanceof HTMLElement) {
+        customHtmlAriaElement.setAttribute(customElement.ariaProp, customElement.ariaValue);
+      } else {
+        console.error("Document error: element is not an HTMLElement:", customHtmlAriaElement);
+      }
 
-  })
+    })
+  }
 
 })  
