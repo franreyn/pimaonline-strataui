90e1873d12e273f8019b9be8dcb032df0bf69368
Clint Mossman <cmossman@pima.edu>
2024-04-16 13:09:15 -0700

    Added custom elements and variables

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -4,10 +4,15 @@ export const customElementsData: { jsxName: string, customHtmlTag: string }[] = 
   { jsxName: "CallOutWidget", customHtmlTag: "callout-widget" },
   { jsxName: "AssignmentsWidget", customHtmlTag: "assignments-widget" },
   { jsxName: "AssignmentItem", customHtmlTag: "assignment-item" },
+  { jsxName: "ContentWrapper", customHtmlTag: "content-wrapper" },
+  { jsxName: "SecondColumn", customHtmlTag: "second-column" },
+  { jsxName: "ContentBody", customHtmlTag: "content-body" },
 ];
 
 // Variable used to know which custom elements  (jsx name, aria property ,aria value)
 export const customElementAria: { customHtmlTag: string, ariaProp: string, ariaValue: string }[] = [
   { customHtmlTag: "assignments-widget", ariaProp: "role", ariaValue: "list" },
   { customHtmlTag: "assignment-item", ariaProp: "role", ariaValue: "listItem" },
+  { customHtmlTag: "content-wrapper", ariaProp: "role", ariaValue: "main" },
+  { customHtmlTag: "second-column", ariaProp: "role", ariaValue: "region" },
 ];
--- src/ts/modules/variables.ts
+++ src/ts/modules/variables.ts
@@ -1,2 +1,5 @@
-export const l: string = "Hi";
-export const m: string = "Hello, again";
+export const contentWrapper: HTMLDivElement | null = document.querySelector("content-wrapper");
+export const courseBody: HTMLBodyElement | null = document.querySelector("body");
+export const secondColumn: HTMLDivElement | null = document.querySelector("second-column");
+export const columnWidget: HTMLDivElement | null = document.querySelector("#column-widget");
+export const videoWrapper: HTMLDivElement | null = document.querySelector("#video-wrapper");
