7b1d1ef66a9fddf1961214961ccfdf1a1aa357ce
Clint Mossman <cmossman@pima.edu>
2024-04-25 15:38:34 -0700

    Edited accordion widget for tsx

--- src/html-templates/_includes/FirstColContent.tsx
+++ src/html-templates/_includes/FirstColContent.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import CallOutWidget from './widgets/CallOutWidget';
 import ContentBlock from './grid-components/ContentBlock';
 import ColumnOne from './grid-components/ColumnOne';
+import { AccordionWidget } from './widgets/AccordionWidget';
+import { AccordionItem } from './widgets/AccordionWidget';
+import { AccordionContent } from './widgets/AccordionWidget';
 
 export default function FirstColContent(): React.ReactElement {
   return (
@@ -10,6 +13,26 @@ export default function FirstColContent(): React.ReactElement {
         <h2>Welcome to [course name here]</h2>
         <CallOutWidget></CallOutWidget>
         <p>Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus. Donec rutrum congue leo eget malesuada.</p>
+        <AccordionWidget>
+          <AccordionItem>
+            <h4>Accordion 1</h4>
+            <AccordionContent>
+              <p>Accordion content 1</p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <h3>Accordion 2</h3>
+            <AccordionContent>
+              <p>Accordion content 2</p>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem>
+            <h3>Accordion 3</h3>
+            <AccordionContent>
+              <p>Accordion content 3</p>
+            </AccordionContent>
+          </AccordionItem>
+        </AccordionWidget>
         <blockquote>Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Sed porttitor lectus nibh. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Nulla porttitor accumsan tincidunt. Curabitur aliquet quam id dui posuere blandit.</blockquote>
         <h2>Header</h2>
         <p>Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Sed porttitor lectus nibh. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Nulla porttitor accumsan tincidunt. Curabitur aliquet quam id dui posuere blandit.</p>
