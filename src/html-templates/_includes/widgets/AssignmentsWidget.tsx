3985a470a3ef81d61cc247b794d46a6d60ab96a1
aphan-webdev <aphan@pima.edu>
2024-06-04 13:15:05 -0700

    Update links in assignments-widget

--- src/html-templates/_includes/widgets/AssignmentsWidget.tsx
+++ src/html-templates/_includes/widgets/AssignmentsWidget.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import customElements from "../CreateCustomElement";
+import ButtonWidget from "./ButtonWidget";
 
 // Accessing AssignmentsWidget and AssignmentItem from customElements
 const AssignmentsWidgetWrapper: React.FC<any> = customElements["AssignmentsWidget"];
@@ -11,17 +12,17 @@ export default function AssignmentsWidget(): React.ReactElement {
       <AssignmentItem>
         <h3>Module 1 Readings</h3>
         <p>The readings in the list have interactive exercises that will help you determine if you are fully understanding the material.</p>
-        <a href="#">Module 1 Readings</a>
+        <ButtonWidget href="#" target="_blank">Module 1 Readings</ButtonWidget>
       </AssignmentItem>
       <AssignmentItem>
         <h3>Reading Review Activity</h3>
         <p>Rhetorical Analysis Review Activity</p>
-        <a href="#">Reading Review Activity</a>
+        <ButtonWidget href="#" target="_blank">Reading Review Activity</ButtonWidget>
       </AssignmentItem>
       <AssignmentItem>
         <h3>Writing Assignment 1</h3>
         <p>Rhetorical Analysis Draft</p>
-        <a href="#">Writing Assignment 1</a>
+        <ButtonWidget href="#" target="_blank">Writing Assignment 1</ButtonWidget>
       </AssignmentItem>
     </AssignmentsWidgetWrapper>
   )
