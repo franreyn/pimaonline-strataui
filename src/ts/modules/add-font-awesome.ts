79a71c2a9f803a4a6cfd9fe54aa96fb3ae1021e8
Clint Mossman <cmossman@pima.edu>
2024-07-16 17:05:27 -0700

    Added types for font awesome link in the head

--- src/ts/modules/add-font-awesome.ts
+++ src/ts/modules/add-font-awesome.ts
@@ -1,13 +1,13 @@
 import { fontAwesomeVersionNum } from "../config";
 
 // Create a link element
-const fontAwesomeLink = document.createElement('link');
+const fontAwesomeLink: HTMLLinkElement = document.createElement('link');
 
 // Get the document head
-const documentHead = document.head;
+const documentHead: HTMLElement = document.head;
 
 // Define the Font Awesome CDN URL
-const fontAwesomeCdn = `https://cdnjs.cloudflare.com/ajax/libs/font-awesome/${fontAwesomeVersionNum}/css/all.min.css`;
+const fontAwesomeCdn: string = `https://cdnjs.cloudflare.com/ajax/libs/font-awesome/${fontAwesomeVersionNum}/css/all.min.css`;
 
 // Add Font Awesome attributes and add the link to the document head
 fontAwesomeLink.setAttribute("href", `${fontAwesomeCdn}`);
