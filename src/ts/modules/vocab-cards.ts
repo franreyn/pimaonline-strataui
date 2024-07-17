87f34cdd61a1f30682aa84fd422138bf44fa36a7
Clint Mossman <cmossman@pima.edu>
2024-07-17 14:25:30 -0700

    Added ts for adding attributes for vocab cards

---
+++ src/ts/modules/vocab-cards.ts
@@ -0,0 +1,23 @@
+const vocabCardsWidgets: NodeListOf<HTMLElement> = document.querySelectorAll("vocab-cards-widget");
+
+
+if (vocabCardsWidgets.length > 0) {
+  // Add aria attributes to ul.vocab-cards
+  vocabCardsWidgets.forEach((vocabCardsWidget: HTMLElement) => {
+    vocabCardsWidget.setAttribute("role", "region");
+    vocabCardsWidget.setAttribute("aria-label", "vocabulary list");
+    vocabCardsWidget.classList.add("vocab-cards-widget");
+
+    const vocabGroups: NodeListOf<HTMLElement> = vocabCardsWidget.querySelectorAll("li,vocab-card");
+
+    if (vocabGroups.length > 0) {
+      // Add aria attributes to li.vocab
+      vocabGroups.forEach((vocabGroup: HTMLElement) => {
+        vocabGroup.setAttribute("role", "group");
+        vocabGroup.setAttribute("aria-label", "vocabulary term and definition");
+      })
+    }
+  })
+
+
+}
