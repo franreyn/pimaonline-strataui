b76d2e04361439acb3a0cca1610eca8e189420d9
Clint Mossman <cmossman@pima.edu>
2024-08-07 14:12:52 -0700

    Change JSX header changes

--- src/html-templates/_includes/Header.tsx
+++ src/html-templates/_includes/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header(): React.ReactElement {
  return (
-    <header className="header">
+    <header>
       <img src="https://via.placeholder.com/1920X600" alt="decorative image of a woman's hand typing on a computer" />
       <div className="text-container">
         <h1>Module 1</h1>
