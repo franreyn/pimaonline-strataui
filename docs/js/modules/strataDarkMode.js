3a0e90140a0e1b2a05d99f01b98c32b52d45fc12
Clint Mossman <cmossman@pima.edu>
2024-08-12 10:18:52 -0700

    Consolidated and edited dark mode for logo and widgets

--- docs/js/modules/strataDarkMode.js
+++ docs/js/modules/strataDarkMode.js
@@ -1,6 +1,7 @@
 const strataBody = document.querySelector("body");
 const strataDarkModeBtn = document.querySelector(".navbar button");
 strataDarkModeBtn.id = "dark-mode-btn";
+const topLogo = document.querySelector('.logo');
 
 // Check the stored dark mode preference on page load
 let strataDarkMode = localStorage.getItem("strataDarkMode") === "true";
@@ -14,6 +15,21 @@ if (strataDarkMode) {
 
 strataDarkModeBtn.addEventListener("click", toggleDarkMode);
 
+// Change the logo based on dark mode
+const darkModeLogo = () => {
+  let strataBody = document.querySelector("body");
+  console.log(strataBody);
+  if (strataBody?.hasAttribute('dark-mode')) {
+    topLogo?.setAttribute('src', '../../_images/full-logo_white.png');
+  } else {
+    topLogo?.setAttribute('src', '../../_images/full-logo_dark.png');
+  }
+};
+
+if(topLogo) {
+  darkModeLogo();
+}
+
 function toggleDarkMode() {
   strataDarkMode = !strataDarkMode;
   // Update the dark mode attribute and button state
@@ -26,4 +42,6 @@ function toggleDarkMode() {
   }
   // Store the current dark mode preference in localStorage
   strataLocalStorage.setItem("strataDarkMode", strataDarkMode);
+  console.log("dark mode toggled")
+  darkModeLogo();
 }
